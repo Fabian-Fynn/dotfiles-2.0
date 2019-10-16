57fcc8ea33ff85293caa4722801774ea98aea499
Fabian Hoffmann <me@fabianhoffmann.io>
2019-10-16 16:34:02 +0200

    Feat: add new aliases and paths

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -8,7 +8,7 @@
 
 # Management
 alias dots='cd ~/dotfiles-2.0 && vim'
-alias ra='cd ~/dotfiles-2.0 && gulp && source ~/.zshrc && echo "reloaded dotfiles"'
+alias ra='cd ~/dotfiles-2.0 && gulp && source ~/.zshrc && cd - && echo "reloaded dotfiles"'
 alias addalias='vim ~/dotfiles-2.0/aliases/aliases.sh'
 alias usebash='chsh -s /bin/bash'
 alias usezsh='chsh -s /bin/zsh'
@@ -24,12 +24,16 @@ alias cd..='cd ..'
 alias mkdir='mkdir -pv' #create parents if needed
 alias mv='mv -i' #move ask if overwrite
 alias tm='tmux new /bin/zsh'
+alias app='cd ~/code/workplace-app'
+alias be='cd ~/code/workplace-backend'
+alias brapp='cd ~/code/blackroll-app'
+alias brbe='cd ~/code/blackroll-backend'
+
 
 # Project navigation
 alias fh='cd ~/Google\ Drive/fh/'
 alias godev='cd ~/Sites'
 alias fodbox='ssh fhoffmann@193.170.119.140 -p 5412'
-alias menow='cd ~/Sites/menow/menow-api/'
 
 # Network
 alias ip="ifconfig|grep broadcast"  # List IPs
@@ -39,6 +43,8 @@ alias fw='~/Sites/inet_studentenheim-master/connect.sh'
 # Git
 alias g='git'
 alias gs='git status'
+alias gsh='git stash'
+alias gsp='git stash pop'
 alias gl='git log'
 alias gw='git show'
 alias gd='git diff HEAD'
@@ -48,7 +54,7 @@ alias gc='git commit --no-verify'
 alias gcm='git commit --no-verify -m'
 alias gpp='git pull --rebase && git push'
 alias gpl='git pull'
-alias gps='git push'
+alias gps='git push origin'
 alias go='git checkout'
 alias gb='git checkout -b'
 alias got='git checkout -'
@@ -56,7 +62,7 @@ alias gom='git checkout master'
 alias gbr='git branch -d'
 alias gcount='eval "git rev-list HEAD --count"'
 alias gra='git remote add'
-alias gbdall='git branch | grep -v "master" | xargs git branch -D'
+alias gbdall='git branch | grep -v "master" | grep -v "development" | xargs git branch -D'
 
 # Tools
 #alias mysql='sudo /usr/local/mysql/support-files/mysql.server'
@@ -87,3 +93,8 @@ export PATH=$PATH:/usr/local/sbin;
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+FIREBASE_TOKEN='1/NQ34S2EQf7UOkcclFa51Onzvbf8FG5R68TubODYFExY'
+
+alias startAndroidEmulator=' ${ANDROID_HOME}/emulator/emulator -avd Nexus_5X_API_29'
+alias rni='react-native run-ios'
+alias rna='react-native run-android'
